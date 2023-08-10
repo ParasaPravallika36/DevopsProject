5ee3cafc870544f8403a5166d196656b6e372911
AdmiN <AdmiN@ITWS02>
2023-08-10 15:28:48 +0530

    another class

---
+++ DevopsProject/src/Com/lbrce/Devops/Test2.java
@@ -0,0 +1,11 @@
+package Com.lbrce.Devops;
+
+public class Test2 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println(" Create a new Class in Devops Project");
+
+	}
+
+}
